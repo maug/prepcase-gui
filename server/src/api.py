139c741c92505d69dcd8ecaf5fa3288800f13dd3
Piotr Kuchta <pkuchta@gmail.com>
2019-10-19 14:31:13 +0200

    Add get_config and mock new_case

--- server/src/api.py
+++ server/src/api.py
@@ -1,12 +1,37 @@
 from flask import Flask
+from flask.helpers import safe_join
 from flask_jsonrpc import JSONRPC
+import os
 
 app = Flask(__name__)
 jsonrpc = JSONRPC(app, '/api', enable_web_browsable_api=True)
 
+THIS_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
+# cesm directory on the server
+CESM_DIRECTORY = THIS_DIRECTORY + "/../../../cesm"
+
+
 @jsonrpc.method('App.list_cases')
 def list_cases():
     return ['case-A', 'case-B', 'case-C']
+
+
+@jsonrpc.method('App.new_case')
+def new_case(name, compset, grid, number_of_instances, multi_driver):
+    # TODO: correct syntax of a call to new_case on command line
+    command = "new_case.sh {} {} {} {} {}".format(name, compset, grid, number_of_instances, multi_driver)
+    # TODO: call new_case.sh
+    return_code = 0
+    return dict(return_code=return_code, command=command)
+
+
+@jsonrpc.method('App.get_config')
+def get_config(path_name):
+    absolute_path_name = safe_join(CESM_DIRECTORY, path_name)
+    with open(absolute_path_name, "r") as f:
+        return dict(path_name=path_name, data=f.read())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- server/src/test.py
+++ server/src/test.py
@@ -2,8 +2,25 @@
 from __future__ import print_function
 
 from flask_jsonrpc.proxy import ServiceProxy
+
 server = ServiceProxy('http://localhost:5000/api')
 
 print ("App.list_cases:")
 for case in server.App.list_cases()['result']:
     print (case)
+
+print ("App.new_case:")
+r = server.App.new_case(name="case-D", compset="B1850", grid="T31_g37", number_of_instances=1, multi_driver=False)['result']
+print(r['command'], '=>', r['return_code'])
+
+
+print ("App.new_case:")
+r = server.App.new_case(name="case-D", compset="B1850", grid="T31_g37", number_of_instances=1, multi_driver=False)['result']
+
+print('App.get_config')
+r = server.App.get_config(path_name="wrong_path_name.xml")
+assert 'error' in r
+r = server.App.get_config(path_name="cime_config/config_compsets.xml")
+assert 'result' in r
+assert 'XML' in str(r['result']['data']).upper()
+
