104765a2e9b8c7599e29a557d726e78f34c6710e
Piotr Kuchta <pkuchta@gmail.com>
2019-10-20 15:19:59 +0200

    Add tools_parameters that returns definitions of accepted parameters of CESM tools. Replace endpoint new_case with more generic run_tool.

--- server/src/api.py
+++ server/src/api.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.helpers import safe_join
 from flask_jsonrpc import JSONRPC
 import os
+import json
 
 app = Flask(__name__)
 jsonrpc = JSONRPC(app, '/api', enable_web_browsable_api=True)
@@ -10,24 +11,45 @@ THIS_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 # cesm directory on the server
 CESM_DIRECTORY = THIS_DIRECTORY + "/../../../cesm"
+CESM_TOOLS = 'create_clone create_test query_testlists create_newcase query_config'.split()
+
+@jsonrpc.method('App.tools_parameters')
+def tools_parameters():
+    """
+    Return definitions of arguments of CESM tools.
+    """
+
+    def read_config(tool):
+        config_file = 'config/' + tool + '_parameters.json'
+        with open(config_file) as f:
+            return json.load(open(config_file))
+
+    return dict([(tool, read_config(tool)) for tool in CESM_TOOLS])
+
+
+@jsonrpc.method('App.run_tool')
+def run_tool(tool, parameters):
+    """
+    Run one of the CESM tools
+    """
+    return_code = 0
+    command = tool + "..." # TODO
+    return dict(return_code=return_code, command=command)
 
 
 @jsonrpc.method('App.list_cases')
 def list_cases():
+    """
+    TODO: List existing user's cases.
+    """
     return ['case-A', 'case-B', 'case-C']
-
-
-@jsonrpc.method('App.new_case')
-def new_case(name, compset, grid, number_of_instances, multi_driver):
-    # TODO: correct syntax of a call to new_case on command line
-    command = "new_case.sh {} {} {} {} {}".format(name, compset, grid, number_of_instances, multi_driver)
-    # TODO: call new_case.sh
-    return_code = 0
-    return dict(return_code=return_code, command=command)
 
 
 @jsonrpc.method('App.get_config')
 def get_config(path_name):
+    """
+    Return content of XML file in CESM directory
+    """
     absolute_path_name = safe_join(CESM_DIRECTORY, path_name)
     with open(absolute_path_name, "r") as f:
         return dict(path_name=path_name, data=f.read())
--- server/src/test.py
+++ server/src/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python 
 from __future__ import print_function
+import json
 
 from flask_jsonrpc.proxy import ServiceProxy
 
@@ -9,13 +10,11 @@ print ("App.list_cases:")
 for case in server.App.list_cases()['result']:
     print (case)
 
-print ("App.new_case:")
-r = server.App.new_case(name="case-D", compset="B1850", grid="T31_g37", number_of_instances=1, multi_driver=False)['result']
+print ("App.run_tool:")
+params = {'--name':"case-D", '--compset':"B1850", '--grid':"T31_g37", '--n-inst':1, '--multi_driver':False}
+r = server.App.run_tool('create_newcase', params)['result']
 print(r['command'], '=>', r['return_code'])
 
-
-print ("App.new_case:")
-r = server.App.new_case(name="case-D", compset="B1850", grid="T31_g37", number_of_instances=1, multi_driver=False)['result']
 
 print('App.get_config')
 r = server.App.get_config(path_name="wrong_path_name.xml")
@@ -24,3 +23,5 @@ r = server.App.get_config(path_name="cime_config/config_compsets.xml")
 assert 'result' in r
 assert 'XML' in str(r['result']['data']).upper()
 
+r = server.App.tools_parameters()
+print(json.dumps(r['result'], indent=4))
