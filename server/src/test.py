fb70a33cda1c3f21af11a4d641ef1194b7c436c7
Piotr Kuchta <pkuchta@gmail.com>
2019-11-02 17:51:37 +0100

    Update test.py

--- server/src/test.py
+++ server/src/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python 
 from __future__ import print_function
 import json
+import sys
 
 from flask_jsonrpc.proxy import ServiceProxy
 
@@ -11,9 +12,14 @@ for case in server.App.list_cases()['result']:
     print (case)
 
 print ("App.run_tool:")
-params = {'--name':"case-D", '--compset':"B1850", '--grid':"T31_g37", '--n-inst':1, '--multi_driver':False}
+params = ["--case", "/home/vagrant/case_test_name_mynior", "--compset", "X", "--res", "T42_T42_mg17", "--machine", "centos7-linux", "--ninst", "1"]
+
 r = server.App.run_tool('create_newcase', params)
 print(r)
+print(r['result']['command'])
+print(r['result']['stdout'])
+print(r['result']['stderr'])
+sys.exit(0)
 
 print('App.get_config')
 r = server.App.get_config(path_name="wrong_path_name.xml")
@@ -25,5 +31,5 @@ assert 'XML' in str(r['result']['data']).upper()
 r = server.App.tools_parameters()
 print(json.dumps(r['result'], indent=4))
 
-r = server.App.run_tool("query_config", {"--compsets": "cam"})
+r = server.App.run_tool("query_config", ["--compsets", "cam"])
 print (r)
