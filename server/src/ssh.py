ac502b60e38d7a4800e3f8d345f648f41c8749db
Marek Augustyn <marek.augustyn@gmail.com>
2020-05-27 15:05:13 +0200

    Change servers' hostnames, add debug info

--- server/src/ssh.py
+++ server/src/ssh.py
@@ -49,7 +49,10 @@ def execute(command):
     p = subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     stdout, stderr = p.communicate()
 
-    return dict(return_code=p.returncode,
-                stdout=stdout,
-                stderr=stderr,
-                command=" ".join(command))
+    result = dict(return_code=p.returncode,
+                  stdout=stdout,
+                  stderr=stderr,
+                  command=" ".join(command))
+
+    print result  # log into /var/log/httpd/prepcase-error.log
+    return result
